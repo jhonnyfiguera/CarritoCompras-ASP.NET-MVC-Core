238b61a840e2e815c7c960b2443fd2d5a0becc97
daichan Miyashiro <dai_miya04@hotmail.com>
2021-04-04 19:28:27 -0300

    agregando clase producto-categoria

---
+++ tp-nt1/Models/Categoria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tp_nt1.Models
+{
+    public class Categoria
+    {
+        #region propiedades
+        public Guid Id { get; private set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public List<Producto> Productos { get; private set; }
+
+
+        #endregion
+
+        #region constructores
+
+        public Categoria(string nombre, string descripcion)
+        {
+            Id = Guid.NewGuid();
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Productos = new List<Producto>();
+        }
+
+        public Categoria():this("sin asignar","sin asignar")
+        {
+        }
+        #endregion
+    }
+}
---
+++ tp-nt1/Models/Producto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tp_nt1.Models
+{
+    public class Producto
+    {
+        #region propiedades
+        public Guid Id { get; private set; }
+
+        public string  Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal PrecioVigente { get; set; }
+
+        public Boolean Activo { get; set; }
+
+        public Categoria Categoria { get; set; }
+        // pendiente FK
+        public Guid CarritoId { get;private set; }
+
+        public Guid CategoriaId { get; private set; }
+
+        #endregion
+
+        #region constructor
+        public Producto(string nombre, string descripcion, decimal precioVigente, Boolean activo, Categoria categoria)
+        {
+            Id = Guid.NewGuid();
+            Nombre = nombre;
+            Descripcion = descripcion;
+            PrecioVigente = precioVigente;
+            Activo = activo;
+            Categoria = categoria;
+            //CarritoId  - CategoriaId    PENDIENTE 
+             
+        }
+
+        public Producto():this("sin asignar","sin asignar",0,false,new Categoria())
+        {
+        }
+
+        #endregion
+    }
+}
