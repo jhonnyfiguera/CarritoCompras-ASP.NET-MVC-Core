d1bf90816946822a94d2193cd5b59f26f994a450
pulidomayerlyn@gmail.com <pulidomayerlyn@gmail.com>
2021-04-04 19:37:01 -0300

    Agregando CarritoItem-StockItem

---
+++ tp-nt1/Models/CarritoItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tp_nt1.Models
+{
+    public class CarritoItem
+    {
+        #region propiedades
+        public Guid Id { get; private set; }
+
+        public Carrito Carrito { get; set; }
+
+        public Producto Producto { get; set; }
+
+        public decimal ValorUnitario { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public  decimal Subtotal { get; set; }
+
+        public Guid CarritoId { get; private set; }
+
+        #endregion
+
+        #region Constructores
+        public CarritoItem(Carrito carrito, Producto producto, decimal valorUnitario, int cantidad, decimal subtotal)
+        {
+            Id = Guid.NewGuid();
+            Carrito = carrito;
+            Producto = producto;
+            ValorUnitario = valorUnitario;
+            Cantidad = cantidad;
+            Subtotal = subtotal;
+            //queda pendiente definir bien carritoId respecto a su FK
+        }
+
+        public CarritoItem():this (new Carrito(), new Producto(), 0, 0, 0)
+
+        {
+        }
+
+
+        #endregion
+    }
+}
---
+++ tp-nt1/Models/StockItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tp_nt1.Models
+{
+    public class StockItem
+    {
+        #region Propiedades
+        public Guid Id { get; private set; }
+
+        public Sucursal Sucursal { get; set; }
+
+        public Producto Producto { get; set; }
+
+        public int Cantidad { get; set; }
+
+        #endregion
+
+        #region Constructores
+
+        public StockItem(Sucursal sucursal, Producto producto, int cantidad)
+        {
+            Id = Guid.NewGuid();
+            Sucursal = sucursal;
+            Producto = producto;
+            Cantidad = cantidad;
+        }
+
+        public StockItem() : this(new Sucursal(), new Producto(), 0)
+        {
+        }
+
+        #endregion
+    }
+}
