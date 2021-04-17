850e8d462228c10273090b38f03e67c42d4a63c8
Jhonny Figuera <figuerajhonny5@gmail.com>
2021-04-17 16:16:43 -0300

    FK Carrito

--- tp-nt1/Models/Carrito.cs
+++ tp-nt1/Models/Carrito.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace tp_nt1.Models
 {
@@ -11,6 +12,8 @@ namespace tp_nt1.Models
 
         public bool Activo { get; set; }
 
+        [ForeignKey(nameof(Cliente))]
+        public Guid ClienteId { get; set; }
         public Cliente Cliente { get; set; }
 
         public List<CarritoItem> CarritosItems { get; set; }
